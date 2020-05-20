98c4760d4bfeb9f5483c7bf2c0392ff652e87cb5
alfonso <alfonso@THINKS>
2020-05-20 11:14:29 +0200

    Document Resource

--- src/main/java/it/tss/projectwork/AbstractEntity.java
+++ src/main/java/it/tss/projectwork/AbstractEntity.java
@@ -29,7 +29,7 @@ public abstract class AbstractEntity implements Serializable {
     protected Long id;
 
     @Column(name = "created_on")
-    protected LocalDateTime createdOn;
+    protected LocalDateTime createdOn = LocalDateTime.now();
 
     @Column(name = "modified_on")
     protected LocalDateTime modifiedOn;
